4c72538250f95b548fe15789571a30388992ec61
IYunet <lbvfzx@gmail.com>
2022-12-10 20:05:54 +0300

    done few API methods

--- platform_site/main_site/models.py
+++ platform_site/main_site/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
 
 
 class DefaultUser(AbstractUser):
-    phone = models.CharField(max_length=30, null=False, verbose_name="Phone")
+    group_choise = (
+        ("S", "Student"),
+        ("C", "Company")
+    )
+    group = models.CharField(max_length=1, blank=False, null=False, verbose_name="Group", choices=group_choise)
+    phone = models.CharField(max_length=30, blank=False, null=False, verbose_name="Phone")
     family_name = models.CharField(max_length=50, blank=True, null=True, verbose_name="Family name")
+    REQUIRED_FIELDS = ["phone", "username", "group"]
+    email = models.EmailField(_('email'), unique=True)
+    USERNAME_FIELD = 'email'
 
 
 class Links(models.Model):
@@ -17,9 +27,10 @@ class Links(models.Model):
 
 class CV(models.Model):
     STATUS_TYPES = (
-        ("A", "Access"),
+        ("A", "Accepted"),
         ("C", "Checking"),
     )
+    link_photo = "https://abrakadabra.fun/uploads/posts/2021-12/1640528651_6-abrakadabra-fun-p-serii-chelovek-na-avu-7.png"
 
     hard_skills = models.TextField(null=False, verbose_name="Hard skills")
     sof_skills = models.TextField(null=False, verbose_name="Soft skills")
@@ -27,7 +38,7 @@ class CV(models.Model):
     name_of_study_institution = models.TextField(null=False, verbose_name="Institution name")
     faculty = models.TextField(null=False, verbose_name="Faculty")
     data_of_end_degree = models.DateField(verbose_name="Degree end")
-    photo = models.TextField(default="https://abrakadabra.fun/uploads/posts/2021-12/1640528651_6-abrakadabra-fun-p-serii-chelovek-na-avu-7.png", verbose_name="Photo")
+    photo = models.TextField(default=link_photo, verbose_name="Photo")
     experience = models.TextField(null=True, verbose_name="Experience")
     status_of_check = models.CharField(max_length=15, choices=STATUS_TYPES, default="C", verbose_name="Checking status")
     link = models.ForeignKey(Links, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Links")
@@ -45,10 +56,12 @@ class Tags(models.Model):
 
 
 class Company(models.Model):
+    link_logo = "https://abrakadabra.fun/uploads/posts/2021-12/1640528651_6-abrakadabra-fun-p-serii-chelovek-na-avu-7.png"
+
     name_company = models.CharField(max_length=100, null=False, verbose_name="Name company")
     address = models.CharField(max_length=100, null=False, verbose_name="Address")
     additional_phone = models.CharField(max_length=30, blank=True, null=True, verbose_name="Additional phone")
-    logo = models.TextField(default="https://abrakadabra.fun/uploads/posts/2021-12/1640528651_6-abrakadabra-fun-p-serii-chelovek-na-avu-7.png", verbose_name="Photo")
+    logo = models.TextField(default=link_logo, verbose_name="Photo")
     user = models.OneToOneField(DefaultUser, on_delete=models.CASCADE, verbose_name="User")
 
 
@@ -60,7 +73,7 @@ class Student(models.Model):
 
     first_name = models.CharField(max_length=100, null=False, verbose_name="First name")
     last_name = models.CharField(max_length=100, null=False, verbose_name="Last name")
-    family_name = models.CharField(max_length=100, null=True, verbose_name="Family name")
+    family_name = models.CharField(max_length=100, blank=True ,null=True, verbose_name="Family name")
     user = models.ForeignKey(DefaultUser, on_delete=models.CASCADE, verbose_name="User")
     birthday = models.DateField(null=False, verbose_name="Birthday")
     cv = models.ForeignKey(CV, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="CV")
@@ -71,4 +84,76 @@ class Student(models.Model):
     is_searching = models.BooleanField(null=False, verbose_name="Searching status")
 
 
+class Rate(models.Model):
+    identification_student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name="Student")
+    work_speed = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],
+                                                  null=True,
+                                                  blank=True,
+                                                  verbose_name='Work speed point')
+    communication = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],
+                                                     null=True,
+                                                     blank=True,
+                                                     verbose_name='Communication point')
+    tech_part = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],
+                                                 null=True,
+                                                 blank=True,
+                                                 verbose_name='Tech part point')
+
+
+
+class CompanyProjects(models.Model):
+    STATUS_TYPES = (
+        ("A", "Accepted"),
+        ("C", "Checking"),
+    )
+
+    name = models.CharField(max_length=100, null=False, verbose_name="Name")
+    description = models.TextField(blank=True, null=True, verbose_name="Photo")
+    fund = models.FloatField(blank=True, null=True, verbose_name="Fund")
+    company_requirements = models.ManyToManyField(Tags, related_name="company_project_tag", verbose_name="Tags")
+    date_start = models.DateField(null=False, verbose_name="Start date")
+    date_end = models.DateField(null=False, verbose_name="End date")
+    checking_status = models.CharField(max_length=1, choices=STATUS_TYPES, default="C", verbose_name="Checking status")
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name="Company")
+
+
+class CompanyBid(models.Model):
+    STATUS_TYPES = (
+        ("A", "Accepted"),
+        ("C", "Checking"),
+    )
+
+    project = models.OneToOneField(CompanyProjects, on_delete=models.CASCADE, verbose_name="Company project")
+    students = models.ManyToManyField(Student, blank=True, null=True, verbose_name="Students")
+    status = models.CharField(max_length=1, choices=STATUS_TYPES, default="C", verbose_name="Checking status")
+    date_start = models.DateField(auto_now_add=True)
+
+
+class StudentProjects(models.Model):
+    STATUS_TYPES = (
+        ("A", "Accepted"),
+        ("C", "Checking"),
+    )
+
+    name = models.CharField(max_length=100, null=False, verbose_name="Name")
+    description = models.TextField(blank=True, null=True, verbose_name="Photo")
+    fund = models.FloatField(blank=True, null=True, verbose_name="Fund")
+    student_requirements = models.ManyToManyField(Tags, related_name="student_project_tag", verbose_name="Tags")
+    date_start = models.DateField(null=False, verbose_name="Start date")
+    date_end = models.DateField(null=False, verbose_name="End date")
+    checking_status = models.CharField(max_length=1, choices=STATUS_TYPES, default="C", verbose_name="Checking status")
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name="Student")
+
+
+class StudentBid(models.Model):
+    STATUS_TYPES = (
+        ("A", "Accepted"),
+        ("C", "Checking"),
+    )
+
+    project = models.OneToOneField(StudentProjects, on_delete=models.CASCADE, verbose_name="Student project")
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Company")
+    students = models.ManyToManyField(Student, blank=True, null=True, verbose_name="Students")
+    status = models.CharField(max_length=1, choices=STATUS_TYPES, default="C", verbose_name="Checking status")
+    date_start = models.DateField(auto_now_add=True)
 
--- platform_site/main_site/views.py
+++ platform_site/main_site/views.py
@@ -1,11 +1,44 @@
 from rest_framework import generics
+from .serializers import *
+from rest_framework import filters
+from .models import *
 
-from main_site.models import DefaultUser
-from main_site.serializers import DefaultUserSerializer
+
+class ProjectsAPIView(generics.ListAPIView):
+    queryset = CompanyProjects.objects.all()
+    serializer_class = ProjectsSerializer
 
 
-class DefaultUserAPIViews(generics.CreateAPIView):
-    queryset = DefaultUser.objects.all()
-    serializer_class = DefaultUserSerializer
-    # def post(self, request, *args, **kwargs):
+class AllStudentsAPIView(generics.ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentsSerializer
 
+
+class GetStudentsByRateAPIView(generics.ListAPIView):
+    queryset = Rate.objects.all()
+    serializer_class = RateSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["work_speed", "communication", "tech_part"]
+
+
+class GetStudentsByTagAPIViews(generics.ListAPIView):
+    serializer_class = StudentsSerializer
+
+    def get_queryset(self):
+        # all_student = Student.objects.all()
+        required_name_tags = self.request.query_params.getlist('tags[]')
+        required_tags = []
+        for required_name_tag in required_name_tags:
+            required_tags.append(Tags.objects.get(name_tag=required_name_tag).pk)
+
+        tags = []
+        queryset = Student.objects.get(tags=required_tags[0])
+        for tag in tags:
+            queryset |= Student.objects.filter(tags=tag)
+
+        print(queryset)
+        if len(queryset):
+            print("**********", queryset)
+            return queryset
+
+        return queryset
